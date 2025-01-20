09ceecde7a24fc41bc23a96615b8c58b31f2f7e7
milkylake <ajnaliev07@gmail.com>
2025-01-20 12:11:01 +0300

    added cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SupabaseService } from './supabase/supabase.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(
+		private readonly appService: AppService,
+		private readonly supabaseService: SupabaseService,
+	) {}
 
 	@Get()
 	getHello(): string {
 		return this.appService.getHello();
+	}
+
+	@Get('test')
+	async getTest() {
+		// const { data, error } = await this.supabaseService.supabase.from('table').select('*');
+		const { data, error } = await this.supabaseService.supabase.from('your_table').insert({ text: 'asd' });
+
+		if (error) throw new Error(error.message);
+		console.log(data);
+		return data;
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(4000);
+	const config = app.get(ConfigService);
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('APPLICATION_ORIGIN'),
+		credentials: true,
+		exposedHeaders: ['set-cookie'],
+	});
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 
 bootstrap();
