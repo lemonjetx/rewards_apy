614d1a9114a51b448faaef412ef7fe7d12d54119
milkylake <ajnaliev07@gmail.com>
2025-01-21 16:34:47 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -2,16 +2,16 @@ create table historical_data
 (
     id           serial primary key,
     date         date           not null default current_timestamp,
-    asset_pool   numeric(30) not null,
-    fee_pool     numeric(30) not null,
-    total_supply numeric(30) not null,
+    asset_pool   numeric(30)    not null,
+    fee_pool     numeric(30)    not null,
+    total_supply numeric(30)    not null,
     token_price  numeric(30, 9) not null
 );
 
 create table apy
 (
-    id          serial primary key,
-    apy         numeric(10, 6) not null,
-    start_date  date           not null,
-    end_date    date           not null
+    id         serial primary key,
+    apy        numeric(10, 6) not null,
+    start_date date           not null,
+    end_date   date           not null
 );
