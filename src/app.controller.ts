0bb288e3e4484104d0fd4c6aa4c32dc2ead67e82
milkylake <ajnaliev07@gmail.com>
2025-01-21 11:03:31 +0300

    added get apy endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,58 +9,8 @@ export class AppController {
 		private readonly supabaseService: SupabaseService,
 	) {}
 
-	@Get()
-	async getHello() {
-		const query = `
-			query GetObject {
-					object(address: "0x0b8c0ef40d9f27a02fdd7cb323d3e89f50e6cd085730fd36be63c587af7053de") {
-						version
-						storageRebate
-						asMoveObject {
-							contents {
-								json
-							}
-						}
-					}
-				}
-		`;
-
-		const res = fetch('https://sui-testnet.mystenlabs.com/graphql', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				query,
-				// variables,
-			}),
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`);
-				}
-				return response.json();
-			})
-			.then((data) => {
-				console.log('GraphQL response:', data);
-				return data;
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-
-		return res;
-
-		return this.appService.getHello();
-	}
-
-	@Get('test')
-	async getTest() {
-		// const { data, error } = await this.supabaseService.supabase.from('table').select('*');
-		// const { data, error } = await this.supabaseService.supabase.from('table').insert({ text: 'asd' });
-
-		// if (error) throw new Error(error.message);
-		// console.log(data);
-		// return data;
+	@Get('apy')
+	async getApy() {
+		return await this.appService.getApy();
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseService } from './supabase/supabase.service';
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		return 'Hello World!';
+	private readonly supabase: SupabaseClient;
+
+	constructor(private readonly supabaseService: SupabaseService) {
+		this.supabase = supabaseService.getClient();
+	}
+
+	async getApy() {
+		const { data, error } = await this.supabase.from('apy').select('*').order('id', { ascending: false }).limit(1);
+
+		if (error || data.length === 0) {
+			console.log(error);
+			return null;
+		}
+
+		return data[0].apy as number;
 	}
 }
